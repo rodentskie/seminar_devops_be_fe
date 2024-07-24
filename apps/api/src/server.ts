eb9b7262f537ee09d9951577003b45467d15dde1
rodentskie <rodentskie@gmail.com>
2024-07-24 21:31:51 +0800

    chore: remove files

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -6,7 +6,7 @@ import { config } from 'dotenv';
 
 import { dbConnect } from '@seminar/pg';
 
-import { employeesRoutes } from './routes';
+// import { employeesRoutes } from './routes';
 
 config();
 
@@ -28,7 +28,7 @@ export const start = async (): Promise<{ server: Server; pool: Pool }> => {
     res.send({ message: 'Hello seminar' });
   });
 
-    app.use('/api/employees', employeesRoutes(pool));
+  // app.use('/api/employees', employeesRoutes(pool));
 
   const server = app.listen(port, host, () => {
     console.log(`[ ready ] http://${host}:${port}`);
