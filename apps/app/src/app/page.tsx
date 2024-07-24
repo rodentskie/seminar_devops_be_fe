ff92250d72778bed3cfd1a8d008973ed7bb85571
rodentskie <rodentskie@gmail.com>
2024-07-25 05:38:59 +0800

    feat: initial app

--- apps/app/src/app/page.tsx
+++ apps/app/src/app/page.tsx
@@ -1,7 +1,21 @@
+import { Container, Flex } from '@mantine/core';
+
+import { ThemeToggle } from '@seminar/theme-toggler';
 export default function Index() {
   return (
     <>
-      <p>Hi</p>
+      <Container fluid p={'xs'}>
+        <Flex
+          mih={50}
+          gap="md"
+          justify="flex-end"
+          align="flex-start"
+          direction="row"
+          wrap="wrap"
+        >
+          <ThemeToggle />
+        </Flex>
+      </Container>
     </>
   );
 }
--- library/components/theme-toggler/src/lib/theme-toggler.tsx
+++ library/components/theme-toggler/src/lib/theme-toggler.tsx
@@ -1,9 +1,25 @@
-export function ThemeToggler() {
+'use client';
+
+import cx from 'clsx';
+import { ActionIcon, useMantineColorScheme, useComputedColorScheme, Group } from '@mantine/core';
+import { IconSun, IconMoon } from '@tabler/icons-react';
+import classes from './style.module.css';
+
+export function ThemeToggle() {
+  const { setColorScheme } = useMantineColorScheme();
+  const computedColorScheme = useComputedColorScheme('light', { getInitialValueInEffect: true });
+
   return (
-    <div>
-      <h1>Welcome to ThemeToggler!</h1>
-    </div>
+    <Group justify="center">
+      <ActionIcon
+        onClick={() => setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')}
+        variant="default"
+        size="xl"
+        aria-label="Toggle color scheme"
+      >
+        <IconSun className={cx(classes.icon, classes.light)} stroke={1.5} />
+        <IconMoon className={cx(classes.icon, classes.dark)} stroke={1.5} />
+      </ActionIcon>
+    </Group>
   );
 }
-
-export default ThemeToggler;
