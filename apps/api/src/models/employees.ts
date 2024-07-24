6a0c81d3c4a5e3da92c3e8a540e885ff405445ea
rodentskie <rodentskie@gmail.com>
2024-07-24 15:10:05 +0800

    chore: model

---
+++ apps/api/src/models/employees.ts
@@ -0,0 +1,23 @@
+import Joi from 'joi';
+
+export const employeeSchema = Joi.object({
+  firstName: Joi.string()
+    .regex(/^[a-zA-Z]+$/)
+    .min(3)
+    .max(30)
+    .required()
+    .messages({
+      'string.pattern.base': 'First name must contain only letters',
+      'string.empty': 'Please enter first name.',
+    }),
+  lastName: Joi.string()
+    .regex(/^[a-zA-Z]+$/)
+    .min(3)
+    .max(30)
+    .required()
+    .messages({
+      'string.pattern.base': 'Last name must contain only letters',
+    }),
+  email: Joi.string().email().required(),
+  age: Joi.number().integer().min(18).max(120).required(),
+});
